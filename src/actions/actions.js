af8460feb2667d7c67cff82f5367a2f0ae7e231b
irene <949415179@qq.com>
2020-04-13 07:55:18 +0800

    init

---
+++ src/actions/actions.js
@@ -0,0 +1,15 @@
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER } from './actions-type'
+let nextTodoId = 0
+export const addTodo = text => ({
+    type: ADD_TODO, 
+    id: nextTodoId++, 
+    text
+})
+export const toggleTodo = id => ({
+    type: TOGGLE_TODO, 
+    id 
+}) 
+export const setVisibilityFilter = filter => ({
+    type: SET_VISIBILITY_FILTER, 
+    filter
+})
